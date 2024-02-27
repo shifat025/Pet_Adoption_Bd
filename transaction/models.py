2327a52eb932bbd4441d1ecf97667b4442bda04d
Shifat-project <shifat32153@gmail.com>
2024-02-27 23:03:18 +0600

    deposit form

--- transaction/models.py
+++ transaction/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 
 class account(models.Model):
     user = models.OneToOneField(User,related_name='account',unique=True, on_delete=models.CASCADE)
-    diposit = models.DecimalField(max_digits=10, decimal_places=2,null=True, blank = True)
-    account_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    diposit = models.DecimalField(max_digits=10, decimal_places=2, default=0,blank = True, null = True)
+    account_balance = models.DecimalField(max_digits=10, decimal_places=2, default=0,blank = True, null = True)
    
     def __str__(self):
         return self.user.username
